f562852f738173e854cf46164b26cc8768fb58f1
jmcimula <jmkacim@gmail.com>
2016-01-16 06:30:16 +0100

    Programming Assignment 2 Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## For the validation the example below
+## p <- makeCacheMatrix(matrix(c(0,2,2,0),2,2))
+## cacheSolve(p)
+## cacheSolve(p)   
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## Initialization the variable inverse to null
+  inverse <- NULL
+  
+  ## Create the setVal function for storing x and inverse 
+  setVal <- function(y) {
+    x <<- y    # Initialization of the matrix
+    inverse <<- NULL
+  }  
+  ## Create getVal function ton return x
+  getVal <- function() x
+  ## Create setinvrs to set the value of inverse to ValInv
+  setinvrs <- function(valInv) inverse <<- valInv
+  ## Create getinvrs to return to retrieve value of inverse
+  getinvrs <- function() inverse
+  
+  # Return the matrix as defined
+  list(setVal = setVal, getVal = getVal,setinvrs = setinvrs, getinvrs = getinvrs)
 }
 
+cacheSolve <- function(x) {
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Check the cache for the inverse
+  valInv <- x$getinvrs()
+  
+  ## Control structure
+  if(!is.null(valInv)) {
+    message("getting cached data")
+    return(valInv)
+  }
+  ## matrix and find the inverse
+  data <- x$getVal()
+  valInv <- solve(data)  
+  
+  ## Cache the inverse
+  x$setinvrs(valInv)
+  
+  ## Return inverse  
+  valInv  
 }
